4c30c50cc9a0033fcde588ea16d64217a113ce66
Orloaft <alexorlow17@gmail.com>
2022-04-29 09:19:04 -0400

    add upload functionality using formData and multer

--- server/index.js
+++ server/index.js
@@ -3,8 +3,51 @@ const app = express();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
+const path = require("path");
 const multer = require("multer"); //module allowing to parse formData
-const upload = multer({ dest: "data/" });
+app.use(cors()); // allow cross origin resource sharing
+app.use(express.static("./public")); //set up static folder
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("myImage");
+
+// Check File Type
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
+
+app.post("/files", upload, (req, res, next) => {
+  // the 'upload' handler thats passed as second parameter handles naming and storing the file -a.o
+  console.log(req.file);
+});
 
 app.listen(8080, () => {
   console.log("server has started on port 8080");
